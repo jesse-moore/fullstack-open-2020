59701ec7d847949291cb43401f1e4a0751d54600
Jesse <moore.jesse@gmail.com>
2020-09-16 06:32:29 -0500

    part2 exercises 2.1 - 2.5

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+export default ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.name} part={part} />
+      ))}
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  const totalExercises = parts.map((e) => e.exercises).reduce((a, b) => a + b);
+  return (
+    <p style={{ fontWeight: "bold" }}>Number of exercises {totalExercises}</p>
+  );
+};
+
+const Part = ({ part }) => {
+  const { name, exercises } = part;
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
