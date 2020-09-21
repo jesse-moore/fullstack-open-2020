9fbd97dd18825542d316b0ab613d6e1612890347
Jesse <moore.jesse@gmail.com>
2020-09-21 15:11:49 -0500

    part2 exercises 3.7 - 3.8

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,8 +1,26 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const phonebook = require("./phonebook");
 
 app.use(express.json());
+morgan.token("body", function (req, res) {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      tokens.body(req, res),
+    ].join(" ");
+  })
+);
 
 app.get("/api/persons", (req, res) => {
   res.json(phonebook);
