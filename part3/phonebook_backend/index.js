cf588ee0f84276f8713d89f297b410d6e21e578e
Jesse <moore.jesse@gmail.com>
2020-09-21 14:15:55 -0500

    part2 exercises 3.3 - 3.6

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 const phonebook = require("./phonebook");
-let notes = [];
+
+app.use(express.json());
 
 app.get("/api/persons", (req, res) => {
   res.json(phonebook);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const entry = phonebook.persons.find(
+    (person) => person.id === Number.parseInt(id)
+  );
+  if (entry) return res.json(entry);
+  res.status(404).end();
 });
 
 app.get("/api/info", (req, res) => {
@@ -14,6 +24,49 @@ app.get("/api/info", (req, res) => {
   const body = `${personsString}\n${timestamp}`;
   res.send(body);
 });
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  const entry = phonebook.persons.find(
+    (person) => person.id === Number.parseInt(id)
+  );
+  if (entry) {
+    deleteEntry(id);
+    return res.status(204).end();
+  }
+  res.status(404).end();
+});
+
+app.post("/api/persons/", (req, res) => {
+  const { name, number } = req.body;
+  if (!name) return res.status(400).send({ error: "name is missing" });
+  if (!number) return res.status(400).send({ error: "number is missing" });
+  if (checkNameExists(name))
+    return res.status(400).send({ error: "name already exists" });
+  const id = Math.floor(Math.random() * 100000);
+  const entry = { id, name, number };
+  addEntry(entry);
+  res.json(entry);
+});
+
+function addEntry(entry) {
+  const { persons } = phonebook;
+  const newPersons = [...persons, entry];
+  phonebook.persons = newPersons;
+}
+
+function deleteEntry(entryID) {
+  const { persons } = phonebook;
+  const newPersons = persons.filter(({ id }) => {
+    return id !== Number.parseInt(entryID);
+  });
+  phonebook.persons = newPersons;
+}
+
+function checkNameExists(name) {
+  const check = phonebook.persons.findIndex((person) => person.name === name);
+  return check === -1 ? false : true;
+}
 
 const PORT = 3001;
 app.listen(PORT, () => {
