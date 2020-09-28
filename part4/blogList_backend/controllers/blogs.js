783edb5bd4076dfb7394b289c31116b5de533559
Jesse <moore.jesse@gmail.com>
2020-09-28 07:08:48 -0500

    part4 exercises 4.13 - 4.14

--- part4/blogList_backend/controllers/blogs.js
+++ part4/blogList_backend/controllers/blogs.js
@@ -6,11 +6,50 @@ blogsRouter.get('/', async (req, res) => {
     res.json(blogs)
 })
 
+blogsRouter.get('/:id', async (req, res) => {
+    const id = req.params.id
+    const blog = await Blog.findById(id)
+    res.json(blog)
+})
+
 blogsRouter.post('/', async (req, res) => {
     const blog = new Blog(req.body)
 
     const result = await blog.save()
     res.status(201).json(result)
+})
+
+blogsRouter.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    await Blog.findByIdAndRemove(id)
+    res.status(204).end()
+})
+
+blogsRouter.put('/:id', async (req, res) => {
+    const id = req.params.id
+    const updates = req.body
+    const updatedBlog = await Blog.findByIdAndUpdate(id, updates, {
+        new: true,
+        runValidators: true,
+        context: 'query',
+    })
+    res.json(updatedBlog)
+})
+
+blogsRouter.put('/:id/inclike', async (req, res) => {
+    const id = req.params.id
+    const { likeInc } = req.body
+    if (!isFinite(likeInc)) throw Error('Bad Request')
+    const updatedBlog = await Blog.findByIdAndUpdate(
+        id,
+        { $inc: { likes: likeInc } },
+        {
+            new: true,
+            runValidators: true,
+            context: 'query',
+        }
+    )
+    res.json(updatedBlog)
 })
 
 module.exports = blogsRouter
--- part4/blogList_backend/tests/blog_api.test.js
+++ part4/blogList_backend/tests/blog_api.test.js
@@ -15,63 +15,133 @@ beforeEach(async () => {
     await Promise.all(savePromises)
 })
 
-test('blogs are returned as json', async () => {
-    await api
-        .get('/api/blogs')
-        .expect(200)
-        .expect('Content-Type', /application\/json/)
+describe('retrieving blogs', () => {
+    test('blogs are returned as json', async () => {
+        await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+    })
+
+    test('unique identifier property of the blog posts is named id', async () => {
+        const response = await api.get('/api/blogs')
+        const blog = response.body[0]
+        expect(blog.id).toBeDefined()
+    })
 })
 
-test('unique identifier property of the blog posts is named id', async () => {
-    const response = await api.get('/api/blogs')
-    const blog = response.body[0]
-    expect(blog.id).toBeDefined()
+describe('creating new blog post', () => {
+    test('successfully creates new blog post', async () => {
+        const newPost = {
+            title: 'New Blog Post',
+            author: 'Dan Young',
+            url: '/',
+            likes: 0,
+        }
+        const response = await api
+            .post('/api/blogs')
+            .send(newPost)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const newPostID = response.body.id
+        const post = await Blog.findById(newPostID)
+        expect(post.title).toBe(newPost.title)
+        expect(post.author).toBe(newPost.author)
+        expect(post.url).toBe(newPost.url)
+        expect(post.likes).toBe(newPost.likes)
+    })
+
+    test('defaults likes to 0', async () => {
+        const newPost = {
+            title: 'New Blog Post',
+            author: 'Dan Young',
+            url: '/',
+        }
+        const response = await api
+            .post('/api/blogs')
+            .send(newPost)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const newPostID = response.body.id
+        const post = await Blog.findById(newPostID)
+        expect(post.likes).toBe(0)
+    })
+
+    test('missing title or author results in 400 Bad Request', async () => {
+        const newPost = {
+            author: 'Dan Young',
+            url: '/',
+        }
+        await api.post('/api/blogs').send(newPost).expect(400)
+    })
 })
 
-test('create new blog post', async () => {
-    const newPost = {
-        title: 'New Blog Post',
-        author: 'Dan Young',
-        url: '/',
-        likes: 0,
-    }
-    const response = await api
-        .post('/api/blogs')
-        .send(newPost)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+describe('updating blog post', () => {
+    test('successfully updates blog post', async () => {
+        const getBlogs = async () => {
+            const response = await api.get('/api/blogs')
+            return response.body
+        }
+        const blogs = await getBlogs()
+        const updateBlogID = blogs[0].id
+        const update = {
+            title: 'Updated Blog Post',
+        }
+        await api
+            .put(`/api/blogs/${updateBlogID}`)
+            .send(update)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
 
-    const newPostID = response.body.id
-    const post = await Blog.findById(newPostID)
-    expect(post.title).toBe(newPost.title)
-    expect(post.author).toBe(newPost.author)
-    expect(post.url).toBe(newPost.url)
-    expect(post.likes).toBe(newPost.likes)
+        const updatedBlogs = await getBlogs()
+        const updatedBlog = updatedBlogs[0]
+        expect(updatedBlog.title).toBe(update.title)
+    })
+
+    test('increments likes', async () => {
+        const getBlogs = async () => {
+            const response = await api.get('/api/blogs')
+            return response.body
+        }
+        const blogs = await getBlogs()
+        const updateBlogID = blogs[0].id
+        const likes = blogs[0].likes
+        const likeInc = {
+            likeInc: 1,
+        }
+        await api
+            .put(`/api/blogs/${updateBlogID}/inclike`)
+            .send(likeInc)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const updatedBlogs = await getBlogs()
+        const updatedBlog = updatedBlogs[0]
+        expect(updatedBlog.likes).toBe(likes + likeInc.likeInc)
+    })
 })
 
-test('defaults likes to 0 when creating new blog post', async () => {
-    const newPost = {
-        title: 'New Blog Post',
-        author: 'Dan Young',
-        url: '/',
-    }
-    const response = await api
-        .post('/api/blogs')
-        .send(newPost)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+describe('deleting blog post', () => {
+    test('successfully deletes blog', async () => {
+        const getBlogs = async () => {
+            const response = await api.get('/api/blogs')
+            return response.body
+        }
+        const blogs = await getBlogs()
+        const blogsCount = blogs.length
+        const blogToDeleteID = blogs[0].id
 
-    const newPostID = response.body.id
-    const post = await Blog.findById(newPostID)
-    expect(post.likes).toBe(0)
-})
+        await api.delete(`/api/blogs/${blogToDeleteID}`).expect(204)
 
-test('missing title or author results in 400 Bad Request', async () => {
-    const newPost = {
-        author: 'Dan Young',
-        url: '/',
-    }
-    await api.post('/api/blogs').send(newPost).expect(400)
+        const blogsAfter = await getBlogs()
+        const blogsCountAfter = blogsAfter.length
+        const blogTitles = blogsAfter.map((b) => b.title)
+
+        expect(blogsCountAfter).toBe(blogsCount - 1)
+        expect(blogTitles).not.toContain(blogs[0].title)
+    })
 })
 
 afterAll(() => {
